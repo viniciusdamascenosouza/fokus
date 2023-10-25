742b57bbfba3c8b1f9879328798396f10b0f87ce
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-25 10:56:23 -0300

    Melhorando código

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ const banner = document.querySelector(".app__image");
 focoBt.addEventListener("click", () => {
   html.setAttribute("data-contexto", "foco");
   banner.setAttribute("src", "./imagens/foco.png");
+  alterarContexto('foco');
 });
 
 curtoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-curto");
-  banner.setAttribute("src", "./imagens/descanso-curto.png");
+    alterarContexto('descanso-curto')
 });
 
 longoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-longo");
-  banner.setAttribute("src", "./imagens/descanso-longo.png");
+    alterarContexto('descanso-longo')
+
 });
+
+function alterarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
+}
