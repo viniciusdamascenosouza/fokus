55b424bed89cb7211ea39c0e137a5815d208b4d9
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-31 17:38:58 -0300

    Criando tarefas estáticas

--- script-crud.js
+++ script-crud.js
@@ -1,1 +1,21 @@
 const taskListContainer = document.querySelector(".app__section-task-list");
+
+let tarefas = [
+  {
+    descricao: "Tarefa concluida",
+    concluida: true,
+  },
+  {
+    descricao: "Tarefa pendente",
+    concluida: false,
+  },
+];
+
+const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="12" cy="12" r="12" 
+    fill="#FFF" />
+<path
+    d "M9 16.1719119.5938 5.57812121 6.9843819 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
+    fill="#01080E" 
+/>`;
