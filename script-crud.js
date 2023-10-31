10c01802c74a238cad26a0661dd742d6a4dea365
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-31 17:49:42 -0300

    Implementando uma função

--- script-crud.js
+++ script-crud.js
@@ -19,3 +19,21 @@ fill="none" xmlns="http://www.w3.org/2000/svg">
     d "M9 16.1719119.5938 5.57812121 6.9843819 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
     fill="#01080E" 
 />`;
+
+function createTask(tarefa) {
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
+
+  const svgIcon = document.createElement("svg");
+  svgIcon.InnerHTML = taskIconSvg;
+
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("app__section-task-list-item-description");
+
+  paragraph.textContent = tarefa.descricao;
+
+  li.appendChild("svgIcon");
+  li.appendChild("paragraph");
+
+  return li;
+}
