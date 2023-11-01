ea40625c9470a55349599d330dd77dbaf65c4206
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-01 15:34:33 -0300

    Mostrando os dados na tela

--- script-crud.js
+++ script-crud.js
@@ -2,23 +2,24 @@ const taskListContainer = document.querySelector(".app__section-task-list");
 
 let tarefas = [
   {
-    descricao: "Tarefa concluida",
+    descricao: "Tarefa Concluída",
     concluida: true,
   },
   {
-    descricao: "Tarefa pendente",
+    descricao: "Tarefa Pendente",
     concluida: false,
   },
 ];
 
-const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
-fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="12" cy="12" r="12" 
-    fill="#FFF" />
-<path
-    d "M9 16.1719119.5938 5.57812121 6.9843819 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
-    fill="#01080E" 
-/>`;
+const taskIconSvg = `
+<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`;
 
 function createTask(tarefa) {
   const li = document.createElement("li");
@@ -34,4 +35,11 @@ function createTask(tarefa) {
 
   li.appendChild(svgIcon);
   li.appendChild(paragraph);
+
+  return li;
 }
+
+tarefas.forEach((task) => {
+  const taskItem = createTask(task);
+  taskListContainer.appendChild(taskItem);
+});
