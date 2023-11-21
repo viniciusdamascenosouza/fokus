891fb256b370b832872a4d776b2d233631205a64
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-21 17:07:21 -0300

    Submetendo o formulário e consumindo a API Json

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
+
+const tarefas = [];
 
 btnAdicionarTarefa.addEventListener("click", () => {
-  formAdicionarTarefa.classList.toggle('hidden')
+  formAdicionarTarefa.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const tarefa = {
+    descricao: textarea.value,
+  };
+  tarefas.push(tarefa);
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
 });
