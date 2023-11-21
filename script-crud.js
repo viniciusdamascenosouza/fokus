c76add73cc082deb4ea9c0c409d4f4b52d4a5140
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-21 17:29:27 -0300

    Lendo tarefas gravadas

--- script-crud.js
+++ script-crud.js
@@ -2,7 +2,9 @@ const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 
-const tarefas = [];
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+const ulTarefas = document.querySelector(".app__section-task-list");
 
 function criarElementoTarefa(tarefa) {
   const li = document.createElement("li");
@@ -32,6 +34,8 @@ function criarElementoTarefa(tarefa) {
   li.append(svg);
   li.append(paragrafo);
   li.append(botao);
+
+  return li;
 }
 
 btnAdicionarTarefa.addEventListener("click", () => {
@@ -45,4 +49,9 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
   };
   tarefas.push(tarefa);
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
+});
+
+tarefas.forEach((tarefa) => {
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefa);
 });
